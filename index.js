b51954829c51ffee98d1a9b00828b9643c9f6d84
givanildosilvatscl <givanildo.silva@tapetessaocarlos.com.br>
2025-09-08 16:09:26 -0300

    Estou adicionando a funcionalidade de cor

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Estou usndo o github');
+import { turboLogger } from './turboLogger.js';
+console.log('Estou usando o github');
+
+turboLogger('estou aprendendo o fluxo do git', 'red');
